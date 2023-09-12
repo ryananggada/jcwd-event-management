037044ce9ad6060c9a9f682f1d0ec38bc9717160
ThePageMode <131966946+thepagemode@users.noreply.github.com>
2023-09-12 15:20:49 +0700

    updated nav buttton

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,31 @@
-import { Box, Image, ButtonGroup, Button, Menu, MenuItem, MenuList, MenuGroup, MenuDivider, MenuButton, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Text, Center } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  ButtonGroup,
+  Button,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuGroup,
+  MenuDivider,
+  MenuButton,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  Text,
+  Center,
+  Heading,
+  Editable,
+  Tooltip,
+  EditablePreview,
+  EditableInput,
+  Input
+} from "@chakra-ui/react"
 import { Link } from "react-router-dom";
 import logoTicket from "../assets/LogoTicket.png";
 import { useSelector, useDispatch } from "react-redux";
@@ -13,8 +40,8 @@ function Navbar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-
+  const { isOpen: isOpenAcc, onOpen: onOpenAcc, onClose: onCloseAcc } = useDisclosure()
+  const { isOpen: isOpenReff, onOpen: onOpenReff, onClose: onCloseReff } = useDisclosure()
 
 
   return (
@@ -43,18 +70,74 @@ function Navbar() {
                 </MenuButton>
                 <MenuList>
                   <MenuGroup title='Profile'>
-                    <MenuItem>My Account</MenuItem>
+                    <MenuItem onClick={onOpenAcc}>Profile</MenuItem>
+                    <Modal isOpen={isOpenAcc} onClose={onCloseAcc}>
+                      <ModalOverlay />
+                      <ModalContent>
+                        <ModalHeader>My Profile</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                          <Heading size='md'>First Name</Heading>
+                          <Editable fontSize='xl'
+                            defaultValue={profile.firstName}
+                            isPreviewFocusable={true}
+                            selectAllOnFocus={false}>
+                            <Tooltip label="Click to edit" shouldWrapChildren={true}>
+                              <EditablePreview
+                                py={2}
+                                px={4}
+                              />
+                            </Tooltip>
+                            <Input py={2} px={4} as={EditableInput} />
+                          </Editable>
+                          <br />
+                          <Heading size='md'>Last Name</Heading>
+                          <Editable fontSize='xl'
+                            defaultValue={profile.lastName}
+                            isPreviewFocusable={true}
+                            selectAllOnFocus={false}>
+                            <Tooltip label="Click to edit" shouldWrapChildren={true}>
+                              <EditablePreview
+                                py={2}
+                                px={4}
+                              />
+                            </Tooltip>
+                            <Input py={2} px={4} as={EditableInput} />
+                          </Editable>
+                          <br />
+                          <Heading size='md'>Username</Heading>
+                          <Editable fontSize='xl'
+                            defaultValue={profile.username}
+                            isPreviewFocusable={true}
+                            selectAllOnFocus={false}>
+                            <Tooltip label="Click to edit" shouldWrapChildren={true}>
+                              <EditablePreview
+                                py={2}
+                                px={4}
+                              />
+                            </Tooltip>
+                            <Input py={2} px={4} as={EditableInput} />
+                          </Editable>
+                        </ModalBody>
+                        <ModalFooter>
+                          <ButtonGroup gap="2">
+                            <Button colorScheme="gray" mr={3} onClick={onCloseAcc}>
+                              Close
+                            </Button>
+                          </ButtonGroup>
+                        </ModalFooter>
+                      </ModalContent>
+                    </Modal>
                     <MenuItem>My Events </MenuItem>
                     <MenuItem>My Tickets </MenuItem>
-                    <MenuItem onClick={onOpen}>Referral Code</MenuItem>
-                    <Modal isOpen={isOpen} onClose={onClose}>
+                    <MenuItem onClick={onOpenReff}>Referral Code</MenuItem>
+                    <Modal isOpen={isOpenReff} onClose={onCloseReff}>
                       <ModalOverlay />
                       <ModalContent>
                         <ModalHeader>Referral Code</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody>
-                          Share this code to get discount!
-                          <br />
+                          <Text textAlign='center'>Share your referral code and get discount for you and your friends when they make first order!</Text>
                           <br />
                           <Box borderWidth='1px'>
                             <Center>
@@ -64,7 +147,7 @@ function Navbar() {
                         </ModalBody>
 
                         <ModalFooter>
-                          <Button colorScheme="gray" mr={3} onClick={onClose}>
+                          <Button colorScheme="gray" mr={3} onClick={onCloseReff}>
                             Close
                           </Button>
                         </ModalFooter>
@@ -91,7 +174,7 @@ function Navbar() {
               <Link to="/login">
                 <Button backgroundColor={"#e38100"}>Login</Button>
               </Link>
-              <Link to="registration-page">
+              <Link to="/registration-page">
                 <Button backgroundColor={"#e38100"}>Sign Up</Button>
               </Link>
             </ButtonGroup>
